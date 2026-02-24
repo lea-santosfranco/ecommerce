992d15c0af3f34aeed014ca12f0d7a0a17f33720
Manondev11 <manon.elm@hotmail.com>
2026-02-24 14:51:49 +0100

    add function homeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $namesStudents=['Léa', 'Kelly', 'Pierrick', 'Manon'];
+        $age =23;
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
