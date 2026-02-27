ba78b59aec6a2cc5a038fba9108d4f36c84a9fa1
Manondev11 <manon.elm@hotmail.com>
2026-02-27 08:13:33 +0100

    maj navbar

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UserController extends AbstractController
+{
+    #[Route('/admin/user', name: 'app_user')]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' =>$userRepository->findAll(),
+        ]);
+    }
+}
