483286d14f2576ea45c563dbd5e14446f4148ccd
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-03-24 08:48:18 +0100

    exercice 16

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,69 +2,86 @@
 
 namespace App\Controller;
 
-use DateTimeImmutable;
 use App\Entity\City;
 use App\Entity\Order;
+use App\Entity\OrderProducts;
 use App\Form\OrderType;
+use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
+use App\Service\Cart;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 final class OrderController extends AbstractController
 {
     #[Route('/order', name: 'app_order')]
-    public function index(Request $request, SessionInterface $session, ProductRepository $productRepository, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $em, SessionInterface $session, ProductRepository $productRepository, Cart $cart): Response
     {
-        $cart = $session->get('cart', []);
-        $cartWithData = [];
-        foreach ($cart as $id => $quantity) {
-            $product = $productRepository->find($id);
-            if ($product) {
-                $cartWithData[] = [
-                    'product' => $productRepository->find($id),
-                    'quantity' => $quantity
-                ];}
-          
-            }     
-        $total = array_sum(array_map(function ($item) {
-            return $item['product']->getPrice() * $item['quantity'];
-        }, $cartWithData));
-        
+        $data = $cart->getCart($session);
+
         $order = new Order();
         $form = $this->createForm(OrderType::class, $order);
-        $form ->handleRequest($request);
+        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $order->setCreatedAt(new DateTimeImmutable());
-            $entityManager->persist($order);
-            $entityManager->flush();
+        if($form->isSubmitted() && $form->isValid()) {
+            if($order->isPayOnDelivery()) {
+
+                if(!empty($data['total'])){
+                    $order->setTotalPrice($data['total']);
+                    $order->setCreatedAt(new DateTimeImmutable());
+
+                    $em->persist($order);
+                    $em->flush();
+                    
+                    foreach($data['cart'] as $value) {
+                        $orderProduct = new OrderProducts();
+                        $orderProduct->setOrder($order);
+                        $orderProduct->setProduct($value['product']);
+                        $orderProduct->setQuantity($value['quantity']);
+                        $em->persist($orderProduct);
+                        $em->flush();
+                    }
+                }
+
+                $session->set('cart', []);
+                return $this->redirectToRoute('app_order_message');
+            }
         }
-
-    return $this->render('order/index.html.twig', [
-            'form' => $form->createView(),
-            'items' => $cartWithData,
-            'total' => $total,
-        ]);
         
-}
-    #[Route ('/city/{id}/shipping/cost', name: 'app_city_shipping_cost')]
+        return $this->render('order/index.html.twig', [
+            'form' => $form->createView(),
+            'total' => $data['total']
+        ]);
+    }
+
+    #[Route('/city/{id}/shipping/cost', name: 'app_city_shipping_cost')]
     public function cityShippingCost(City $city): Response
     {
         $cityShippingPrice = $city->getShippingCost();
 
-        return new Response(json_encode([
-            'status' => 200,
-            'message' => 'on',
-            'content' => $cityShippingPrice
-        ]));
+        return new Response(json_encode(['status'=>200, 'message'=>'on', 'content'=>$cityShippingPrice]));
+    }
+
+    #[Route('/order/message', name:'app_order_message')]
+    public function orderMessage():Response
+    {
+        return $this->render('order/order_message.html.twig');
+    }
+
+    #[Route('/editor/order', name:'app_orders_show')]
+    public function getAllOrder(OrderRepository $orderRepository):Response
+    {
+        $orders = $orderRepository->findAll();
+
+        return $this->render('order/order_list.html.twig', [
+            'orders' => $orders
+        ]);
     }
 }
-
-
-
-
-
