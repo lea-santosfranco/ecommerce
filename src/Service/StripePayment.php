3ea723c6689c73979f3d320935458a82c1dd0cfa
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-03-26 08:32:40 +0100

    modif stripepayment

--- src/Service/StripePayment.php
+++ src/Service/StripePayment.php
@@ -11,15 +11,13 @@ class StripePayment
 
    public function __construct()
    {
-        Stripe::setApiKey($_SERVER['STRIPE_SECRET_KEY']); //recupère la cle secrete dans le fichier .env gràce au $_server
-        Stripe::setApiVersion('2024-06-20'); //on gère la version de Stripe
+        Stripe::setApiKey($_SERVER['STRIPE_SECRET_KEY']);
+        Stripe::setApiVersion('2024-06-20');
     }
 
     public function startPayment($cart, $shippingCost, $orderId){
-        //dd($cart);
-       // Récupération des produits du panier
         $cartProducts = $cart['cart']; 
-        // Initialisation d'un tableau vide pour stocker les produits formatés
+
         $products = [
             [
                 'qte' => 1, 
@@ -28,22 +26,21 @@ class StripePayment
             ]
         ];
 
-        // Boucle pour parcourir chaque produit du panier
         foreach ($cartProducts as $value) {
-            // Initialisation d'un tableau vide pour stocker les informations d'un produit
+            
             $productItem = [];
-            // Récupération du nom du produit
+            
             $productItem['name'] = $value['product']->getName();
-            // Récupération du prix du produit
+           
             $productItem['price'] = $value['product']->getPrice();
-            // Récupération de la quantité du produit
+            
             $productItem['qte'] = $value['quantity'];
-            // Ajout du produit formaté au tableau des produits
+           
             $products[] = $productItem;
         }
 
-        $session = Session::create([ //création de la session Stripe
-            'line_items'=>[  //produits qui vont etre payer
+        $session = Session::create([ 
+            'line_items'=>[ 
                 array_map(fn(array $product) => [
                     'quantity' => $product['qte'],
                     'price_data' => [
@@ -51,29 +48,29 @@ class StripePayment
                         'product_data' => [
                            'name' => $product['name']
                         ],
-                        'unit_amount' => $product['price']*100, //prix donnée en centimes donc on multiplie
+                        'unit_amount' => $product['price']*100, 
                     ],
                 ],$products)
             ],
-            'mode' => 'payment', //mode de paiement
-            'cancel_url' => 'http://localhost:8000/pay/cancel', //si paiement annulé on redirige ici
-            'success_url' => 'http://localhost:8000/pay/success', //si paiement réussi
-            'billing_address_collection' => 'required', //si on autorise les factures
-            'shipping_address_collection' => [ //pays ou on souhaite autorise le paiement
+            'mode' => 'payment', 
+            'cancel_url' => 'http://localhost:8000/pay/cancel', 
+            'success_url' => 'http://localhost:8000/pay/success', 
+            'billing_address_collection' => 'required', 
+            'shipping_address_collection' => [ 
                 'allowed_countries' => ['FR','EG'],
             ],
             'payment_intent_data' => [
                 'metadata' => [
-                    'orderId' =>$orderId//id de la commande
+                    'orderId' =>$orderId
                 ]
             ]
             
         ]); 
 
-        $this->redirectUrl = $session->url; //redirection vers stripe pour le paiement
+        $this->redirectUrl = $session->url;
 
     }
-    public function getStripeRedirectUrl(){ //permet de recuperer l'url de l'utilisateur pour stripe
+    public function getStripeRedirectUrl(){
         return $this->redirectUrl;
     }
 }
